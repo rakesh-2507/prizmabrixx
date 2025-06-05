4128a8f996f5e5cb3641a1d49b0e5ef670e88566
Rakesh Raj <161500341+rakesh-2507@users.noreply.github.com>
2025-06-05 13:43:54 +0530

    Delete server directory

--- server/routes/callRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const callController = require('../controllers/callController');
-
-router.post('/call', callController.initiateCall);
-
-module.exports = router;
